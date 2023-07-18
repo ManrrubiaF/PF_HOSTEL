77e8ea464351acfc94db355ed9b1ba93fc0471fd
maxinunezz <maxicbapl@gmail.com>
2023-07-18 20:13:30 -0300

    tired

--- Client/src/Store/HotelsStore.ts
+++ Client/src/Store/HotelsStore.ts
@@ -93,54 +93,6 @@ export const hotelStore = create<States & Actions>((set) => ({
       }));
     }
   },
-  setCurrentPage: (pageNum) => {
-    set(() => ({
-      currentPage: pageNum,
-    }));
-  },
-  orderByName: (array, event) => {
-    if (event === "ASC") {
-      const arrayAux = array.sort(function (a, b) {
-        if (a.name > b.name) {
-          return 1;
-        }
-        if (a.name < b.name) {
-          return -1;
-        }
-        return 0;
-      });
-      set((state) => ({
-        ...state,
-        hotels: arrayAux,
-      }));
-    } else if (event === "DES") {
-      const arrayAux = array.sort((a, b) => b.name.localeCompare(a.name));
-      set((state) => ({
-        ...state,
-        hotels: arrayAux,
-      }));
-    }
-  },
-  orderByCategory: (array, event) => {
-    if (event === "ASC") {
-      const arrayAux = array.sort(
-        (a, b) => parseInt(a.hotelCategory) - parseInt(b.hotelCategory)
-      );
-      set((state) => ({
-        ...state,
-        hotels: arrayAux,
-      }));
-    } else if (event === "DES") {
-      const arrayAux = array.sort(
-        (a, b) => parseInt(b.hotelCategory) - parseInt(a.hotelCategory)
-      );
-      set((state) => ({
-        ...state,
-        hotels: arrayAux,
-      }));
-    }
-  },
-
   resetHotels: () => {
     set(initialState);
   },
--- Client/vite.config.ts
+++ Client/vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -9,13 +8,16 @@ export default defineConfig({
       name: "configure-response-headers",
       configureServer: (server) => {
         server.middlewares.use((_req, res, next) => {
-          res.setHeader("Cross-Origin-Opener-Policy", "same-origin-allow-popups");
-          res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
-        res.setHeader('Access-Control-Allow-Credentials', 'true');
+          res.setHeader(
+            "Cross-Origin-Opener-Policy",
+            "same-origin-allow-popups"
+          );
+          res.setHeader("Access-Control-Allow-Origin", "http://localhost:5173");
+          res.setHeader("Access-Control-Allow-Credentials", "true");
           next();
         });
       },
     },
     react(),
   ],
-})
+});
