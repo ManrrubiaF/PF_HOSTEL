2a836d9f043ea57758312170ef590957bf224101
ManrrubiaF <manrrubiaf@gmail.com>
2025-06-11 11:45:48 -0300

    fixing relative rute FormPageHotelUpdate

--- Client/src/Pages/FormPageH/FormPageHotelUpdate.tsx
+++ Client/src/Pages/FormPageH/FormPageHotelUpdate.tsx
@@ -7,7 +7,7 @@ import BackButton from "../../components/BackButton/BackButton";
 import { ArrowCounterClockwise } from "@phosphor-icons/react";
 import axios from "axios";
 import { errorToast, successToast } from "../../components/toast";
-import Hotel from "./Hotel.png";
+import Hotel from "./hotel.png";
 import { hotelStore, tokenStore } from "../../Store";
 import { useNavigate, useParams } from "react-router-dom";
 import { servicesHotel } from "../../utils/servicesArray";
